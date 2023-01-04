fe9d41413466f99a0d88146adee3e2ef17c435e0
akocur <akocur@yandex.ru>
2023-01-04 21:53:21 +0300

    refactoring a procedure from an exercise 1.3

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -4,13 +4,13 @@
 
 (define (square x) (* x x))
 
-(define (sum-of-squares x1 x2 x3)
+(define (sum-of-two-largest-squares x1 x2 x3)
  (+ (square (max x1 x2)) (square (max (min x1 x2) x3))))
 
-(check-equal? (sum-of-squares 1 2 3) 13)
-(check-equal? (sum-of-squares 1 3 2) 13)
-(check-equal? (sum-of-squares 3 2 1) 13)
-(check-equal? (sum-of-squares 2 3 1) 13)
-(check-equal? (sum-of-squares 2 1 3) 13)
-(check-equal? (sum-of-squares 3 1 2) 13)
-(check-equal? (sum-of-squares 0 3 -3) 9)
+(check-equal? (sum-of-two-largest-squares 1 2 3) 13)
+(check-equal? (sum-of-two-largest-squares 1 3 2) 13)
+(check-equal? (sum-of-two-largest-squares 3 2 1) 13)
+(check-equal? (sum-of-two-largest-squares 2 3 1) 13)
+(check-equal? (sum-of-two-largest-squares 2 1 3) 13)
+(check-equal? (sum-of-two-largest-squares 3 1 2) 13)
+(check-equal? (sum-of-two-largest-squares 0 3 -3) 9)
