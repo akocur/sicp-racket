29724cd3fb0240f41de0239a961e556cb006a941
akocur <akocur@yandex.ru>
2023-04-09 21:17:35 +0300

    add sicp/chapter1/06.rkt

---
+++ sicp/chapter1/06.rkt
@@ -0,0 +1,28 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (sqrt x) (sqrt-iter 1 x))
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+
+(define (square x) (* x x))
+
+(define (improve guess x)
+ (average guess (/ x guess)))
+
+(define (average x y)
+    (/ (+ x y) 2.0))
+
+(check-equal? (average 3 2) 2.5)
+(check-equal? (square 3) 9)
+(check-equal? (good-enough? 2 9) #f)
+(check-equal? (good-enough? 3.0001 9) #t)
+(check-equal? (improve 1 2) 1.5)
+(check-within (sqrt 9) 3 0.001)
