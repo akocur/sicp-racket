5c13ff4d24738368d9b5cadcc38a7f3e4f2b09bf
akocur <akocur@yandex.ru>
2023-05-08 19:38:17 +0300

    solve the exercise 1.6

--- sicp/chapter1/06.rkt
+++ sicp/chapter1/06.rkt
@@ -5,7 +5,7 @@
 (define (sqrt x) (sqrt-iter 1 x))
 
 (define (sqrt-iter guess x)
-    (if (good-enough? guess x)
+    (if-new (good-enough? guess x) ;даже когда good-enough? вернет #t третий параметр продолжит вычисляться.
         guess
         (sqrt-iter (improve guess x) x)))
 
@@ -20,9 +20,13 @@
 (define (average x y)
     (/ (+ x y) 2.0))
 
+(define (if-new predicate then-case else-case)
+    (cond (predicate then-case)
+        (else else-case)))
+
 (check-equal? (average 3 2) 2.5)
 (check-equal? (square 3) 9)
 (check-equal? (good-enough? 2 9) #f)
 (check-equal? (good-enough? 3.0001 9) #t)
 (check-equal? (improve 1 2) 1.5)
-(check-within (sqrt 9) 3 0.001)
+;(check-within (sqrt 9) 3 0.001) не выполнится. будет бесконечная рекурсия из за аппликативного порядка вычисления.
