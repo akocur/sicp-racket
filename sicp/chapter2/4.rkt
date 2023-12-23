4e3320dfac9086644fafc37966718ea6c3229b32
akocur <akocur@yandex.ru>
2023-12-23 17:15:53 +0300

    fix lint error

--- sicp/chapter2/4.rkt
+++ sicp/chapter2/4.rkt
@@ -2,11 +2,9 @@
 
 (#%require rackunit)
 
-#|review: ignore|#
-
 (define (cons x y) (lambda (f) (f x y)))
-(define (car z) (z (lambda (p q) p)))
-(define (cdr z) (z (lambda (p q) q)))
+(define (car z) (z (lambda (p _) p)))
+(define (cdr z) (z (lambda (_ q) q)))
 
 (check-equal? (car (cons 2 3)) 2)
 (check-equal? (cdr (cons 2 3)) 3)
